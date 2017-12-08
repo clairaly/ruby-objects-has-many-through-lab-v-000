a936f0462470d3acbea08a68a669e37678127c11
Alyssa Clair <clairaly13@gmail.com>
2017-12-08 21:44:20 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,21 +1,22 @@
 class Patient
   attr_accessor :name, :appointments
 
-  def initialize(name)
-    @name = name
-    @appointments = []
+ def initialize(name)
+   @name = name
+   @appointments = []
+ end
+
+ def appointments
+    @appointments
   end
 
-  def add_appointment
+  def add_appointment(appointment)
     @appointments << appointment
     appointment.patient = self
   end
 
-  def appointments
-    @appointments
-  end
-
   def doctors
-     self.appointments.collect {|appointment| appointment.doctor }
+    self.appointments.collect {|appointment| appointment.doctor }
+
   end
 end
