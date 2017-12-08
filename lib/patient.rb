4a7e9d330097d0417fea16fff3ac9519ea9c65d0
Alyssa Clair <clairaly13@gmail.com>
2017-12-08 21:41:57 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,4 +6,15 @@ class Patient
     @appointments = []
   end
 
+  def add_appointment
+    @appointments << appointment
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+     self.appointments.collect {|appointment| appointment.doctors }
+  end
 end
